ffe1363c19f96a2317230b138414682d5641df42
漂读网 <piaoduwang@zhangtiancai.local>
2018-12-18 09:27:30 +0800

    Initial Commit

---
+++ DQM_SQLiteManagerDemoTests/DQM_SQLiteManagerDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  DQM_SQLiteManagerDemoTests.m
+//  DQM_SQLiteManagerDemoTests
+//
+//  Created by 漂读网 on 2018/12/18.
+//  Copyright © 2018 漂读网. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DQM_SQLiteManagerDemoTests : XCTestCase
+
+@end
+
+@implementation DQM_SQLiteManagerDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
